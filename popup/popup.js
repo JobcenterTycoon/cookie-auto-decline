1c39281462a841af4d15eddb178b3e9579365ab5
JobcenterTycoon <JobcenterTycoon@gmail.com>
2025-09-03 19:55:55 +0200

    Füge Englisch zum Popup hinzu + kleine verbesserungen

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,6 @@
 'use-strict';
 
+const ungültigedomains = ['accounts-static.cdn.mozilla.net', 'accounts.firefox.com', 'addons.cdn.mozilla.net', 'addons.mozilla.org', 'api.accounts.firefox.com', 'content.cdn.mozilla.net', 'discovery.addons.mozilla.org', 'oauth.accounts.firefox.com', 'profile.accounts.firefox.com', 'support.mozilla.org', 'sync.services.mozilla.com', '127.0.0.1', '0.0.0.0', '[::]', '[::1]', 'localhost', 'fritz.box', 'fritz.nas', 'fritz.repeater', 'myfritz.box', 'giga.cube', 'congstar.box', 'easy.box', 'kabel.box', 'pi.hole', 'speedport.ip', 'my.router', 'home.arpa'];
 const suchstatus = document.getElementById('suchstatus');
 const suchstatuscookie = document.getElementById('suchstatuscookie');
 const anbieter = document.getElementById('anbieter');
@@ -9,9 +10,70 @@ const knopfstatuscontainer = document.getElementById('knopfstatuscontainer');
 const main = document.getElementById('main');
 const logo = document.getElementById('logo');
 const keineseite = document.getElementById('keineseite');
+const keineseitetext = document.getElementById('keineseitetext');
 const zufrühgeöffnet = document.getElementById('zufrühgeöffnet');
+const zufrühgeöffnettext = document.getElementById('zufrühgeöffnettext');
+const anbietertext = document.getElementById('anbietertext');
+const einwilligungsstatustext = document.getElementById('einwilligungsstatustext');
+const erweiterteerkennungtext = document.getElementById('erweiterteerkennungtext');
+
 let wiederholungverhindern;
 let domain;
+
+// Deutsch oder Englisch auswählen
+let knopfstatusablehntext;
+let knopfstatusgeschlossentext;
+let knopfstatusakzeptierttext;
+let knopfstatuseinstellungtext;
+let knopfstatusgespeicherttext;
+let knopfstatusdefaulttext;
+
+let suchstatusnichtausgeführt;
+let suchstatussuche;
+let suchstatusnichtsgefunden;
+let suchstatusgefunden;
+let suchstatusfehler;
+
+if (navigator.language === 'de' || navigator.language.startsWith('de-')) {
+   suchstatuscookie.innerText = 'Cookie oder LocalStorage gesetzt.';
+   anbietertext.innerText = 'Cookie Banner Anbieter:';
+   einwilligungsstatustext.innerText = 'Einwilligungsstatus:'
+   keineseitetext.innerText = 'Nicht unterstützte Seite. Das Addon ist auf dieser Seite dauerhaft deaktiviert.';
+   zufrühgeöffnettext.innerText = 'Fehler beim Abrufen des Status. Öffne das Popup neu für einen neuen Versuch.';
+   erweiterteerkennungtext.innerHTML = 'Erweiterte Cookie Banner Erkennung (beta): ';
+   knopfstatusablehntext = 'Abgelehnt.';
+   knopfstatusgeschlossentext = 'Geschlossen.';
+   knopfstatusakzeptierttext = 'Akzeptiert.';
+   knopfstatuseinstellungtext = 'Einstellungen geöffnet.';
+   knopfstatusgespeicherttext = 'Einstellungen gespeichert.';
+   knopfstatusdefaulttext = 'Knopf nicht gefunden.';
+
+   suchstatusnichtausgeführt = 'Nicht ausgeführt.';
+   suchstatussuche = 'Suche...';
+   suchstatusnichtsgefunden = 'Kein Cookie Banner gefunden.';
+   suchstatusgefunden = 'Cookie Banner gefunden.';
+   suchstatusfehler = 'Fehler';
+} else {
+   suchstatuscookie.innerText = 'Set cookie or localStorage';
+   anbietertext.innerText = 'Cookie banner provider:';
+   einwilligungsstatustext.innerText = 'Accept status:'
+   keineseitetext.innerText = 'Unsupported site. The addon is disabled on this site.';
+   zufrühgeöffnettext.innerText = 'Error while fetching the status. Reopen this popup for a new try.';
+   erweiterteerkennungtext.innerHTML = 'Advanced cookie banner detection (beta): ';
+   knopfstatusablehntext = 'Declined.';
+   knopfstatusgeschlossentext = 'Closed.';
+   knopfstatusakzeptierttext = 'Accepted.';
+   knopfstatuseinstellungtext = 'Settings opened.';
+   knopfstatusgespeicherttext = 'Settings saved.';
+   knopfstatusdefaulttext = 'Button not found.';
+
+   suchstatusnichtausgeführt = 'Not executed.';
+   suchstatussuche = 'Searching...';
+   suchstatusnichtsgefunden = 'No cookie banner found.';
+   suchstatusgefunden = 'Cookie banner found.';
+   suchstatusfehler = 'Error';
+}
+// ENDE - Deutsch oder Englisch auswählen - ENDE
 
 browser.tabs.query({
    currentWindow: true,
@@ -30,13 +92,24 @@ browser.tabs.query({
          main.style.display = 'none';
          logo.src = "icon_64_off.png";
          logo.style.pointerEvents = 'none';
-         zufrühgeöffnet.style.display = 'block';
+         if (ungültigedomains.toString().includes(domain) === false) {
+            zufrühgeöffnet.style.display = 'block';
+         }
       });
       // Logo updaten je nach dem ob die Seite valid (http) oder invalid ist.
-      main.style.display = 'block';
-      keineseite.style.display = 'none';
-      logo.src = "icon_64.png";
-      logo.style.pointerEvents = 'auto';
+
+      if (ungültigedomains.toString().includes(domain) === false) {
+         main.style.display = 'block';
+         keineseite.style.display = 'none';
+         logo.src = "icon_64.png";
+         logo.style.pointerEvents = 'auto';
+      } else {
+         main.style.display = 'none';
+         keineseite.style.display = 'block';
+         logo.src = "icon_64_off.png";
+         logo.style.pointerEvents = 'none';
+         zufrühgeöffnet.style.display = 'none';
+      }
    } else {
       main.style.display = 'none';
       keineseite.style.display = 'block';
@@ -59,19 +132,19 @@ browser.runtime.onMessage.addListener(function (message) {
       // Suchstatus auf dem Popup anzeigen
       switch (message.nachricht.suchstatus) {
          case 'nicht ausgeführt':
-            suchstatus.innerText = 'Nicht ausgeführt.';
+            suchstatus.innerText = suchstatusnichtausgeführt;
             break;
          case 'suche':
-            suchstatus.innerText = 'Suche...';
+            suchstatus.innerText = suchstatussuche;
             break;
          case 'nichts gefunden':
-            suchstatus.innerText = 'Kein Cookie Banner gefunden.';
+            suchstatus.innerText = suchstatusnichtsgefunden;
             break;
          case 'gefunden':
-            suchstatus.innerText = 'Cookie Banner gefunden.';
+            suchstatus.innerText = suchstatusgefunden;
             break;
          default:
-            suchstatus.innerText = 'error';
+            suchstatus.innerText = suchstatusfehler;
             break;
       }
 
@@ -96,22 +169,22 @@ browser.runtime.onMessage.addListener(function (message) {
 
       switch (message.nachricht.knopfstatus) {
          case 'abgelehnt':
-            knopfstatus.innerText = 'Abgelehnt.';
+            knopfstatus.innerText = knopfstatusablehntext;
             break;
          case 'geschlossen':
-            knopfstatus.innerText = 'Geschlossen.';
+            knopfstatus.innerText = knopfstatusgeschlossentext;
             break;
          case 'akzeptiert':
-            knopfstatus.innerText = 'Akzeptiert.';
+            knopfstatus.innerText = knopfstatusakzeptierttext;
             break;
          case 'einstellungen':
-            knopfstatus.innerText = 'Einstellungen geöffnet.';
+            knopfstatus.innerText = knopfstatuseinstellungtext;
             break;
          case 'gespeichert':
-            knopfstatus.innerText = 'Einstellungen gespeichert.';
+            knopfstatus.innerText = knopfstatusgespeicherttext;
             break;
          default:
-            knopfstatus.innerText = 'Knopf nicht gefunden.';
+            knopfstatus.innerText = knopfstatusdefaulttext;
             break;
       }
    }
