3fe95b91b2acb3387245f24636b4873e0719d337
JobcenterTycoon <JobcenterTycoon@gmail.com>
2025-10-08 17:20:46 +0200

    Erhöhung der Zuverlässigkeit der Anzeige im Popup

--- popup/popup.js
+++ popup/popup.js
@@ -12,7 +12,6 @@ const knopfstatus = document.getElementById('knopfstatus');
 const knopfstatuscontainer = document.getElementById('knopfstatuscontainer');
 const main = document.getElementById('main');
 const keineseite = document.getElementById('keineseite');
-const zufrühgeöffnet = document.getElementById('zufrühgeöffnet');
 const schaltercheckbox = document.getElementById('schaltercheckbox');
 const schalterhintergrund = document.getElementById('schalterhintergrund');
 
@@ -26,14 +25,23 @@ function schalteranimationsofort(a) {
    }
 }
 
-let wiederholungverhindern;
+function invalidedomain() {
+   main.style.display = 'none';
+   keineseite.style.display = 'block';
+   schalteranimationsofort(true);
+   schaltercheckbox.checked = false;
+   schalterhintergrund.style.pointerEvents = 'none';
+}
+
 let domain;
+let deaktiviertaufderseite = false;
+let statusanzeigen;
 
 // localization code
 [...document.querySelectorAll('[data-i18n]')].forEach(e => {
-   e[e.dataset.i18nValue || 'textContent'] = chrome.i18n.getMessage(e.dataset.i18n);
+   e[e.dataset.i18nValue || 'textContent'] = browser.i18n.getMessage(e.dataset.i18n);
    if (e.dataset.i18nTitle) {
-      e.title = chrome.i18n.getMessage(e.dataset.i18nTitle);
+      e.title = browser.i18n.getMessage(e.dataset.i18nTitle);
    }
 });
 
@@ -42,25 +50,93 @@ browser.tabs.query({
    active: true,
 }).then(function (tabs) {
    if (tabs[0].url.startsWith('http')) {
-      // Ändere URL zu Domain und entfernen das www sowie www mit Zahlen (z.b. www2)
+      // Ändere Tab URL zu Domain und entfernen das www sowie www mit Zahlen (z.b. www2)
       domain = tabs[0].url.replace(/^https?:\/\/(www([0-9]{1,2})?\.)?/, '').replace(/\/.*/, '');
-      // Popup Kommunikation
-      browser.tabs.sendMessage(tabs[0].id, {
-         popupstatus: "geöffnet"
-      }).then(function () {
-         console.log("[Cookie auto decline] geöffnet nachricht vom popup script an den content script gesendet");
-      }).catch(function () {
-         console.error("[Cookie auto decline] Kein Content Script erreichbar.");
-         main.style.display = 'none';
-         schalteranimationsofort(true);
-         schaltercheckbox.checked = false;
-         schalterhintergrund.style.pointerEvents = 'none';
-         if (ungültigedomains.toString().includes(domain) === false) {
-            zufrühgeöffnet.style.display = 'block';
-         }
-      });
-      // Logo updaten je nach dem ob die Seite valid (http) oder invalid ist.
 
+      statusanzeigen = function () {
+         // Status anzeigen
+         browser.storage.local.get('cookiebannerstatuscookie').then(function (a) {
+            if (a && a.cookiebannerstatuscookie) {
+               let invalideseite = true;
+               for (let i = 0; i < a.cookiebannerstatuscookie.length; i++) {
+                  if (a.cookiebannerstatuscookie[i].seite === domain) {
+                     invalideseite = false;
+                     // "Cookie oder LocalStorage gesetzt" anzeigen
+                     if (a.cookiebannerstatuscookie[i].cookieoderstoragegesetzt) {
+                        suchstatuscookie.style.display = 'block';
+                     }
+                     // Suchstatus auf dem Popup anzeigen
+                     switch (a.cookiebannerstatuscookie[i].suchstatus) {
+                        case 'nicht ausgeführt':
+                           suchstatus.innerText = _('searchStatusNotExecutedMessage');
+                           break;
+                        case 'suche':
+                           suchstatus.innerText = _('searchStatusSearchingMessage');
+                           break;
+                        case 'nichts gefunden':
+                           suchstatus.innerText = _('searchStatusNothingFoundMessage');
+                           break;
+                        case 'gefunden':
+                           suchstatus.innerText = _('searchStatusFoundMessage');
+                           break;
+                        default:
+                           suchstatus.innerText = _('searchStatusErrorMessage');
+                           break;
+                     }
+
+                     // Anbieter auf dem Popup anzeigen
+                     if (a.cookiebannerstatuscookie[i].anbieter !== 'unbekannt') {
+                        anbieter.innerText = a.cookiebannerstatuscookie[i].anbieter;
+                        anbietercontainer.style.display = 'block';
+                     } else {
+                        anbietercontainer.style.display = 'none';
+                     }
+
+                     // Knopfstatus auf dem Popup anzeigen
+                     if (a.cookiebannerstatuscookie[i].anbieter !== 'unbekannt' || a.cookiebannerstatuscookie[i].suchstatus === 'gefunden') {
+                        knopfstatuscontainer.style.display = 'block';
+                     } else {
+                        knopfstatuscontainer.style.display = 'none';
+                     }
+
+                     if (a.cookiebannerstatuscookie[i].anbieter === _('providerSelfProgrammedMessage')) {
+                        knopfstatuscontainer.style.display = 'none';
+                     }
+
+                     switch (a.cookiebannerstatuscookie[i].knopfstatus) {
+                        case 'abgelehnt':
+                           knopfstatus.innerText = _('buttonStatusDeclinedMessage');
+                           break;
+                        case 'geschlossen':
+                           knopfstatus.innerText = _('buttonStatusClosedMessage');
+                           break;
+                        case 'akzeptiert':
+                           knopfstatus.innerText = _('buttonStatusAcceptedMessage');
+                           break;
+                        case 'einstellungen':
+                           knopfstatus.innerText = _('buttonStatusSettingsOpenedMessage');
+                           break;
+                        case 'gespeichert':
+                           knopfstatus.innerText = _('buttonStatusSettingsSavedMessage');
+                           break;
+                        default:
+                           knopfstatus.innerText = _('buttonStatusDefaultMessage');
+                           break;
+                     }
+                  }
+               }
+               if (invalideseite === true && !deaktiviertaufderseite) {
+                  invalidedomain();
+               }
+            } else if (!deaktiviertaufderseite) {
+               invalidedomain();
+            }
+         });
+      };
+      // statusanzeigen();
+      browser.storage.local.onChanged.addListener(statusanzeigen);
+
+      // Addon als ausgeschaltet anzeigen wenn die Domain eine geschütte Mozilla Domain ist.
       if (ungültigedomains.toString().includes(domain) === false) {
          main.style.display = 'block';
          keineseite.style.display = 'none';
@@ -68,91 +144,10 @@ browser.tabs.query({
          schaltercheckbox.checked = true;
          schalterhintergrund.style.pointerEvents = 'auto';
       } else {
-         main.style.display = 'none';
-         keineseite.style.display = 'block';
-         schalteranimationsofort(true);
-         schaltercheckbox.checked = false;
-         schalterhintergrund.style.pointerEvents = 'none';
-         zufrühgeöffnet.style.display = 'none';
+         invalidedomain();
       }
    } else {
-      main.style.display = 'none';
-      keineseite.style.display = 'block';
-      schalteranimationsofort(true);
-      schaltercheckbox.checked = false;
-      schalterhintergrund.style.pointerEvents = 'none';
-   }
-});
-
-browser.runtime.onMessage.addListener(function (message) {
-   // Verhindern das der selbe Wert immer wieder neu ins Popup geschrieben wird.
-   if (wiederholungverhindern && message.nachricht && message.nachricht.suchstatus === wiederholungverhindern.suchstatus && message.nachricht.anbieter === wiederholungverhindern.anbieter && message.nachricht.knopfstatus === wiederholungverhindern.knopfstatus) {
-      return;
-   }
-   wiederholungverhindern = message.nachricht;
-   // Aktualisiere Status
-   if (message.nachricht && message.nachricht.cookiegesetzt === true) {
-      suchstatuscookie.style.display = 'block';
-
-   } else if (message.nachricht && message.nachricht.suchstatus) {
-      // Suchstatus auf dem Popup anzeigen
-      switch (message.nachricht.suchstatus) {
-         case 'nicht ausgeführt':
-            suchstatus.innerText = _('searchStatusNotExecutedMessage');
-            break;
-         case 'suche':
-            suchstatus.innerText = _('searchStatusSearchingMessage');
-            break;
-         case 'nichts gefunden':
-            suchstatus.innerText = _('searchStatusNothingFoundMessage');
-            break;
-         case 'gefunden':
-            suchstatus.innerText = _('searchStatusFoundMessage');
-            break;
-         default:
-            suchstatus.innerText = _('searchStatusErrorMessage');
-            break;
-      }
-
-      // Anbieter auf dem Popup anzeigen
-      if (message.nachricht.anbieter !== 'unbekannt') {
-         anbieter.innerText = message.nachricht.anbieter;
-         anbietercontainer.style.display = 'block';
-      } else {
-         anbietercontainer.style.display = 'none';
-      }
-
-      // Knopfstatus auf dem Popup anzeigen
-      if (message.nachricht.anbieter !== 'unbekannt' || message.nachricht.suchstatus === 'gefunden') {
-         knopfstatuscontainer.style.display = 'block';
-      } else {
-         knopfstatuscontainer.style.display = 'none';
-      }
-
-      if (message.nachricht.anbieter === _('providerSelfProgrammedMessage')) {
-         knopfstatuscontainer.style.display = 'none';
-      }
-
-      switch (message.nachricht.knopfstatus) {
-         case 'abgelehnt':
-            knopfstatus.innerText = _('buttonStatusDeclinedMessage');
-            break;
-         case 'geschlossen':
-            knopfstatus.innerText = _('buttonStatusClosedMessage');
-            break;
-         case 'akzeptiert':
-            knopfstatus.innerText = _('buttonStatusAcceptedMessage');
-            break;
-         case 'einstellungen':
-            knopfstatus.innerText = _('buttonStatusSettingsOpenedMessage');
-            break;
-         case 'gespeichert':
-            knopfstatus.innerText = _('buttonStatusSettingsSavedMessage');
-            break;
-         default:
-            knopfstatus.innerText = _('buttonStatusDefaultMessage');
-            break;
-      }
+      invalidedomain();
    }
 });
 
@@ -186,6 +181,7 @@ browser.storage.local.get("aufdiesenseitendeaktiviert").then(function (a) {
             schalteranimationsofort(true);
             schaltercheckbox.checked = false;
             schalterhintergrund.title = _('clickToEnable');
+            deaktiviertaufderseite = true;
          }
       }
    }
@@ -224,4 +220,5 @@ browser.storage.local.get("aufdiesenseitendeaktiviert").then(function (a) {
          contentscriptausgeführt: true
       });
    });
+   statusanzeigen();
 });
